57fdcd1b7c593df33e9618c3a663fafecb5e7b2a
jRocha494 <a21280321@isec.pt>
2022-01-01 22:39:25 +0000

    Model

---
+++ src/com/model/Model.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class Model {
+    Reservation reservation;
+
+//    public boolean addReservation(){}
+//    public boolean editReservation(){}
+//    public boolean verifyFormat(){}
+//    public boolean cancelReservation(){}
+//    public String viewReservations(){}
+//    public boolean refuel(){}
+//    public String verifySlot(){}
+//    public String viewStatistics(){}
+//    public String calcMostCommonCat(){}
+//    public String calcMostActiveMonth(){}
+//    public String calcIncome(){}
+//    public String calcMostProfitMonth(){}
+}
