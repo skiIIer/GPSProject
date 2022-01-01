2f67f6323f8d6f3b9043bb0540532f9868b47c01
jRocha494 <a21280321@isec.pt>
2022-01-01 19:53:13 +0000

    GetsSetsReservations

--- src/com/model/Reservation.java
+++ src/com/model/Reservation.java
@@ -1,5 +1,7 @@
 package com.model;
 
+import java.sql.Date;
+
 public class Reservation {
     String clientName;
     Date checkInDate;
@@ -8,4 +10,60 @@ public class Reservation {
     long nif;
     String registrationNumber;
     int state;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public float getBill() {
+        return bill;
+    }
+
+    public void setBill(float bill) {
+        this.bill = bill;
+    }
+
+    public long getNif() {
+        return nif;
+    }
+
+    public void setNif(long nif) {
+        this.nif = nif;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
 }
