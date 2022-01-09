942e526714e66416c986a7f272db52df7adeaf62
Filipe <a21280323@isec.pt>
2022-01-09 22:44:17 +0000

    mostCommonCat a funcionar

--- src/com/view/Main.java
+++ src/com/view/Main.java
@@ -146,15 +146,17 @@ public class Main {
 
     public static void main(String[] args) {
         model = new Model();
+        /*
         if(model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-11-31"), 15, 234124721, "AS-27-SD", 2, "Large"))
             System.out.println("Reservation successfully made.");
         else
             System.out.println("\nNo slots available for the specified date.");
-        model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-11-30"), 15, 234124721, "AS-27-SD", 2, "Small");
-        model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-10-11"), 15, 234124721, "AS-27-SD", 2, "Medium");
-        model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-10-11"), 15, 234124721, "AS-27-SD", 2, "Medium");
-        model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-9-23"), 15, 234124721, "AS-27-SD", 2, "Large");
-        model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-11-23"), 15, 234124721, "AS-27-SD", 2, "Small");
+        */
+        //model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-11-30"), 11, 234124721, "AS-27-SD", 2, "Small");
+        //model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-10-11"), 12, 234124721, "AS-27-SD", 2, "Medium");
+        //model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-10-11"), 13, 234124721, "AS-27-SD", 2, "Medium");
+        //model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-9-23"), 14, 234124721, "AS-27-SD", 2, "Large");
+        //model.addReservation("To Manel", Date.valueOf("2010-03-01"), Date.valueOf("2030-11-23"), 15, 234124721, "AS-27-SD", 2, "Small");
         //model.addReservation("To Manel", Date.valueOf("2027-03-01"), Date.valueOf("2029-11-31"), 15.2, 234124721, "AS-27-SD", 2, "Medium");
 
         //model.addReservation("Bob", Date.valueOf("2022-04-23"), Date.valueOf("2023-02-12"), 15.2, 521124721, "SA-27-SD", 2);
